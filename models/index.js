fd16af6038aa5a1fe60d9168d43e81b4609bee46
gab2112 <gabriel.vgonzalez@alumnos.udg.mx>
2022-12-12 09:33:02 -0600

    Subir back

---
+++ models/index.js
@@ -0,0 +1,18 @@
+
+
+const Categoria = require('./categoria');
+const Nivel = require('./niveles');
+const Role = require('./role');
+const Server = require('./server');
+const Usuario = require('./usuario');
+
+
+
+module.exports = {
+    Categoria,
+    Nivel,
+    Role,
+    Server,
+    Usuario,
+}
+
